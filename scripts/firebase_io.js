681cff9e6578bdb496d4038fbf088311f7b2c7c4
Nguyen Huu <nguyenhuu4242@gmail.com>
2021-03-08 19:49:21 +0900

    active status display

--- scripts/firebase_io.js
+++ scripts/firebase_io.js
@@ -26,34 +26,50 @@ function fetchCandidates()
         console.log(`avatars length = ${avatars.length}`);
         snapshot.forEach((doc) => {
             candidates.push(doc.id);
+            console.log("candidates.length "+candidates.length);
             var data = doc.data();
             console.log(`${doc.id} => ${data.avatar}, ${data.started_date}, ${data.rejected_date}`);
+            
             var avatar = avatars[i];
             avatar.src = data.avatar;
+            if(data.rejected_date < Date.now())
+            {
+                avatar.style = "filter: grayscale(80%);";
+            }
             i++;
         });
+        console.log("candidates.length check "+candidates.length);
+        if(candidates.length > 0)
+        {
+            current_candidate = 0;
+            fetchVotes();
+        }
     });
-    if(candidates.length > 0)
-    {
-        current_candidate = 0;
-    }
+    
 }
 
 function fetchVotes()
 {
+    console.log("current_candidate "+candidates[current_candidate]);
     if(current_candidate == -1)
     {
         return;
     }
+    console.log(`fetch vote for /candidates/${candidates[current_candidate]}`);
     var db = firebase.firestore();
-    var query = db.collection("votes").where("vote_for", "==", "candidates/"+candidates[current_candidate]);
-    var normal_avg = 0;
-    var normal_count = 0;
-    var power_avg = 0;
-    var power_count = 0;
+    const candidate = db.collection('candidates').doc(candidates[current_candidate]);
+    var query = db.collection("votes").where("vote_for", "==", candidate);
+    
     query.get().then((snapshot) => {
+        var vote_for = null;
+        var normal_avg = 0;
+        var normal_count = 0;
+        var power_avg = 0;
+        var power_count = 0;
         snapshot.forEach((doc) => {
             var data = doc.data();
+            console.log(`data ${data.vote_for.id}`);
+            vote_for = data.vote_for;
             if(data.power_vote)
             {
                 power_avg += data.rating;
@@ -65,29 +81,45 @@ function fetchVotes()
                 normal_count += 1;
             }
         });
-    });
-    var power = power_count>0?power_avg/power_count:0;
-    var normal = normal_count>0?normal_avg/normal_count:0;
-    var count = power_count>0?1:0 + normal_count>0?1:0;
-    var score = count>0?(power + normal)/count:0;
-    
-    var status = document.getElementById("status-text")
-    status.classList = [];
-    status.classList.add("level-"+Math.floor(score));
-    
-    var styles = document.styleSheets[0];
-    for (var i in styles.cssRules)
-    {
-        if (styles.cssRules[i].name === 'needle-animation-'+current_candidate)
+        console.log("power_avg "+power_avg);
+        var power = power_count>0?power_avg/power_count:0;
+        var normal = normal_count>0?normal_avg/normal_count:0;
+        var count = power_count>0?1:0 + normal_count>0?1:0;
+        var score = count>0?(power + normal)/count:0;
+
+        console.log("score "+score);
+        
+        var status = document.getElementById("status-text")
+        status.classList = [];
+        status.classList.add("level-"+Math.floor(score));
+        
+        var styles = document.styleSheets[0];
+        for (var i in styles.cssRules)
         {
-            var toFrame = styles.cssRules[i].cssRules[1];
-            var angle = score*180/5;
-            toFrame.style.transform = 'rotate(' + angle + 'deg)';
-            break;
+            if (styles.cssRules[i].name === 'needle-animation-'+current_candidate)
+            {
+                console.log("found animation needle-animation-"+current_candidate);
+                var toFrame = styles.cssRules[i].cssRules[1];
+                var angle = score*180/5;
+                toFrame.style.transform = 'rotate(' + angle + 'deg)';
+                break;
+            }
         }
-    }
-    var needle = document.getElementById('needle-img');
-    needle.style.animationName = 'needle-animation-'+current_candidate;
+        var needle = document.getElementById('needle-img');
+        needle.style.animationName = 'needle-animation-'+current_candidate;
+        var avatars = document.getElementById("avatar").getElementsByTagName("img");
+        for (var i in avatars)
+        {
+            avatars[i].classList = i==current_candidate?["selected"]:[];
+        }
+        candidate.get().then((doc) => {
+            var active_status = document.getElementById('active-status');
+            const rejected = doc.data().rejected_date.toDate() < Date.now();
+            console.log("doc.data().rejected_date "+doc.data().rejected_date);
+            active_status.classList = rejected?["inactive"]:["active"];
+        });
+        
+    });
 }
 
 function switchCandidate(index)
